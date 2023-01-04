3d0ebdfc79765509bd8fae90a7c4f42047307ec0
Richard Gao <85811699+GamingPiglet@users.noreply.github.com>
2023-01-04 02:48:04 +0000

    Bug fixes after testing

--- main.py
+++ main.py
@@ -8,59 +8,58 @@ print("Welcome " + os.getenv("REPL_OWNER")) # prints a little welcome message fo
 currenttime = datetime.datetime.now(pytz.timezone("America/Toronto"))
 day = currenttime.day % 2
 
-if (not "Period 3" in db.keys()):
-	print("What would you like to do?")
+if (not "Period 4" in db.keys()):
+	print("What would you like to do?\n")
 	while True:
 		print("1\t-\tMake Schedule")
 		print("2\t-\tEnd Program")
-		print("Command:", end=" ")
-		command = sys.stdin.readline().trim()
+		command = input("Command: ")
 		if command == "1":
 			print("Please enter your classes in the order you have them today:")
-			db["Period 1"] = sys.stdin.readline().trim()
-			db["Period 2"] = sys.stdin.readline().trim()
-			db["Period " + str(3 + day)] = sys.stdin.readline().trim()
-			db["Period " + str(4 - day)] = sys.stdin.readline().trim()
+			db["Period 1"] = sys.stdin.readline().strip()
+			db["Period 2"] = sys.stdin.readline().strip()
+			db["Period " + str(4 - day)] = sys.stdin.readline().strip()
+			db["Period " + str(3 + day)] = sys.stdin.readline().strip()
 			print("Schedule successfully saved.")
-			schedule = db["Period 1"] + "\n" + db["Period 2"] + "\n" + db["Period " + str(3 + day)] + "\n" + db["Period " + str(4 - day)] + "\n"
+			schedule = "1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n"
 			break
 		elif command == "2":
 			print("Thank you for using the program.")
-			sys.exit()
+			break
 		else:
 			print("Invalid input, please try again.")
 else:
-	schedule = "1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(3 + day)] + "\n" + "4\t-\t" + db["Period " + str(4 - day)] + "\n"
+	schedule = "1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n"
 	print("Here's today's schedule: \n\n" + schedule)
-
-print("What would you like to do?")
-while True:
+	
+while "Period 4" in db.keys():
+	print("What would you like to do?")
 	print("1\t-\tEdit Schedule")
 	print("2\t-\tPrint schedule")
 	print("3\t-\tEnd Program")
-	print("Command:", end=" ")
-	command = sys.stdin.readline().trim()
+	command = input("Command: ")
 	if command == "1":
 		print("Here's your current schedule:")
 		print(schedule)
-		print("Please enter the number of the class you want to replace:", end=" ")
-		edit = sys.stdin.readline().trim()
-		print("Please enter the replacement class:", end=" ")
+		edit = input("Please enter the number of the class you want to replace: ")
 		if edit == "1" or edit == "2":
-			db["Period " + edit] = sys.stdin.readline().trim()
+			db["Period " + edit] = input("Please enter the replacement class: ")
 			print("Class replaced successfully.")
+			schedule = "1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n"
 		elif edit == "3":
-			db["Period " + str(3 + day)] = sys.stdin.readline().trim()
+			db["Period " + str(4 - day)] = input("Please enter the replacement class: ")
 			print("Class replaced successfully.")
+			schedule = "1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n"
 		elif edit == "4":
-			db["Period " + str(4 - day)] = sys.stdin.readline().trim()
+			db["Period " + str(3 + day)] = input("Please enter the replacement class: ")
 			print("Class replaced successfully.")
+			schedule = "1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n"
 		else:
 			print("Invalid input, going back to command menu...")
-	elif command == 2:
+	elif command == "2":
 		print("Here's today's schedule: \n\n" + schedule)
-	elif command == 3:
+	elif command == "3":
 		print("Thank you for using the program.")
-		sys.exit()
+		break
 	else:
 		print("Invalid input, please try again.")
