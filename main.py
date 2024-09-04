8e44846d144e8b921ef1c0ae10e81de1a6d565d6
GamingPiglet <85811699+GamingPiglet@users.noreply.github.com>
2024-09-04 20:53:33 +0000

    Remove unnecessary files from repository and add .gitignore

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ currenttime = datetime.datetime.now(pytz.timezone("America/Toronto"))
 day = currenttime.day % 2
 credentials = service_account.Credentials.from_service_account_info(json.loads(os.getenv("API_CREDENTIALS")), scopes=["https://www.googleapis.com/auth/calendar"]) # initialize credentials from credentials secret
 client = build('calendar', 'v3', discoveryServiceUrl="https://calendar-json.googleapis.com/$discovery/rest?version=v3", credentials=credentials) # set up google calendar api client to decide what times classes occur. i could just not use the discoveryServiceUrl field but for some reason it can't find the api without it
-daysoff = client.events().list(calendarId="c_d3boa08h5mvnj1c7bb218hj6ao@group.calendar.google.com", maxResults=1, singleEvents=True, timeMin=datetime.datetime.utcnow().isoformat() + "Z").execute()["items"]
-latestarts = client.events().list(calendarId="81964c8f563eda3f4ecaad55e66e5fa3b9afb6a3452539bf6146497b82914283@group.calendar.google.com", maxResults=1, singleEvents=True, timeMin=datetime.datetime.utcnow().isoformat() + "Z").execute()["items"]
-
-if "Period 4" in db.keys():
-	print("Here's today's schedule: \n")
-	print("1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n")
+daysoff = client.events().list(calendarId="c_d3boa08h5mvnj1c7bb218hj6ao@group.calendar.google.com", maxResults=1,  singleEvents=True, timeMin=datetime.datetime.utcnow().isoformat() + "Z").execute()["items"]
+dayoffbegin = datetime.datetime.combine(datetime.datetime.strptime(daysoff[0]["start"]["date"], "%Y-%m-%d"), datetime.datetime.min.time())
+dayoffend = datetime.datetime.combine(datetime.datetime.strptime(daysoff[0]["start"]["date"], "%Y-%m-%d"), datetime.datetime.max.time())
+if "elementary" in daysoff[0]["summary"] and not "secondary" in daysoff[0]["summary"]:
+	dayoffbegin = datetime.datetime.combine(datetime.datetime.strptime(daysoff[1]["start"]["date"], "%Y-%m-%d"), datetime.datetime.min.time())
+	dayoffend = datetime.datetime.combine(datetime.datetime.strptime(daysoff[1]["start"]["date"], "%Y-%m-%d"), datetime.datetime.max.time())
 
 while True:
 	print("What would you like to do?")
@@ -67,8 +67,25 @@ while True:
 			else:
 				print("Going back...")
 		elif command == "3":
-			print("Here's today's schedule: \n")
-			print("1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n")
+			if currenttime.month == 9:
+				firstday = client.events().list(calendarId="c_d3boa08h5mvnj1c7bb218hj6ao@group.calendar.google.com", q="first day of school", singleEvents=True, timeMin=str(currenttime.year)+ "-01-01T00:00:00.000000Z").execute()["items"]
+				comparefirstday = datetime.datetime.combine(datetime.datetime.strptime(firstday[0]["start"]["date"], "%Y-%m-%d"), datetime.datetime.min.time())
+				if currenttime >= comparefirstday:
+					print("Here's today's schedule: \n")
+					print("1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n")
+				else:
+					print("No school today.")
+			elif currenttime.month == 7 or currenttime.month == 8:
+				print("No school today.")
+			elif len(daysoff) > 0 and currenttime >= dayoffbegin and currenttime <= dayoffend:
+				if  daysoff[0]["summary"].lower():
+					print("Here's today's schedule: \n")
+					print("1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n")
+				else :
+					print("No school.")
+			else:
+				print("Here's today's schedule: \n")
+				print("1\t-\t" + db["Period 1"] + "\n" + "2\t-\t" + db["Period 2"] + "\n" + "3\t-\t" + db["Period " + str(4 - day)] + "\n" + "4\t-\t" + db["Period " + str(3 + day)] + "\n")
 		elif command == "4":
 			print("Thank you for using the program.")
 			break
